8aa33043b8e62c75d707e7013370dcf699e13808
rakib <rakibulislamr83@gmail.com>
2025-11-07 23:52:45 +0600

    url update v2

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -132,7 +132,8 @@ const LoginPage = () => {
             <button
               onClick={() =>
                 signIn("github", {
-                  callbackUrl: "http://localhost:3000/dashboard",
+                  callbackUrl:
+                    "https://next-auth-client-taupe.vercel.app/dashboard",
                 })
               }
               className="flex items-center justify-center w-12 h-12 bg-gray-100 rounded-full shadow-md hover:bg-gray-200"
